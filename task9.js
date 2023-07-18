a1cd9415b2c16d6620992f87420ea2c9231a03f4
Yashprasadpatil <yashprasadp@gmail.com>
2023-07-18 20:42:41 +0530

    task9

---
+++ task9.js
@@ -0,0 +1,5 @@
+//Event Loop
+
+// => The Node.js event loop is responsible for handling all the asynchronous code execution in the application.
+//It uses a mechanism called "callbacks" to handle the events and execute the code associated with them.
+//When a callback is called, it is added to the event loop queue, and the event loop will execute it when it reaches the callback in the queue.
